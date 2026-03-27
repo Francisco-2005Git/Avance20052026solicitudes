bbdac421373a56373325cfc7a469e7c113544618
DanieICC <cardozacalderondaniel400@gmail.com>
2026-03-27 00:52:13 -0600

    se movieron unos archivos, y ya

--- Administrador.php
+++ Administrador.php
@@ -36,7 +36,7 @@ $stmtUsuarios->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Solicitudes - Administrador — ITSRV</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="layout-app">
 
